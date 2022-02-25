7af3ca7fad7f2b111e0ab1695c106bbe407ece95
Kanchana46 <kanchana.rdg@gmail.com>
2022-02-25 10:44:40 +0530

    first commit

---
+++ src/test/java/com/code/k8demo/K8demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.k8demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
